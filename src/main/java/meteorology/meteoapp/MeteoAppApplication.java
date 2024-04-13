ac15d4bed05a39d40744cf0512dd53552721c053
DESKTOP-PBSAHHN\Laptoop <n.lenktis@gmail.com>
2024-04-13 13:17:20 +0300

    Initial state

---
+++ src/main/java/meteorology/meteoapp/MeteoAppApplication.java
@@ -0,0 +1,13 @@
+package meteorology.meteoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/meteorology/meteoapp/MeteoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package meteorology.meteoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
